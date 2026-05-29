167dcbd3dc0bec5521e6530bc798e124fc586ed7
Grendel <jcg@grendel.world>
2026-05-29 16:19:55 -0700

    Add more completion settings

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -20,15 +20,19 @@ vim.opt.listchars = {
     trail = '§',
     leadmultispace = '│⋅⋅⋅',
 }
+vim.api.nvim_create_autocmd('OptionSet', {
+    group = vim.api.nvim_create_augroup('OptionSetAugroup', { clear = true }),
+    pattern = 'shiftwidth',
+    callback = function()
+        vim.opt.listchars:append({
+            leadmultispace = '│' .. string.rep('·', vim.o.shiftwidth - 1)
+        })
+    end,
+})
 
 vim.o.pumblend = 0
 vim.o.pumborder = 'rounded'
 vim.o.winborder = 'rounded'
-
-vim.opt.wildignore:append({ '*.o', '*.a', '*.obj', '*.class', '*.s' })
-vim.o.wildmenu = true
-vim.o.wildmode = 'full'
-vim.opt.wildoptions = { 'fuzzy', 'pum' }
 
 -- editing settings
 vim.o.autocomplete = true
@@ -42,6 +46,19 @@ vim.opt.cinwords:append({ 'case', 'elif', 'match' })
 vim.opt.clipboard:append('unnamedplus')
 vim.opt.completeopt:append({ 'fuzzy', 'menu', 'menuone', 'noselect', 'preview' })
 
+-- completion settings
+vim.o.wildmenu = true
+vim.opt.wildignore:append({ '*.o', '*.a', '*.obj', '*.class', '*.s' })
+vim.opt.wildmode = { 'noselect:lastused', 'full' }
+vim.opt.wildoptions = { 'fuzzy', 'pum' }
+
+vim.api.nvim_create_autocmd('CmdlineChanged', {
+    group = vim.api.nvim_create_augroup('CmdlineAugroup', { clear = true }),
+    pattern = { ':', '/', '?' },
+    callback = function() vim.fn.wildtrigger() end,
+})
+
+
 -- netrw settings
 vim.g.netrw_liststyle = 3     -- tree style listing
 vim.g.netrw_browse_split = 4  -- open file in previous window
@@ -49,14 +66,3 @@ vim.g.netrw_banner = 0        -- no banner
 vim.g.netrw_usetab = 1        -- use netrw-<C-Tab> mapping
 vim.g.netrw_wiw = 32          -- window width (cols)
 vim.g.shada = "'0f0"          -- what to save in the ShaDa file
-
--- autocmds
-vim.api.nvim_create_autocmd('OptionSet', {
-    group = vim.api.nvim_create_augroup('OptionSetAugroup', { clear = true }),
-    pattern = 'shiftwidth',
-    callback = function()
-        vim.opt.listchars:append({
-            leadmultispace = '│' .. string.rep('·', vim.o.shiftwidth - 1)
-        })
-    end,
-})
