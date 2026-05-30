63adb136bd0d54b2171cf9e11fb8969113ebe9d2
Grendel <jcg@grendel.world>
2026-05-29 18:48:34 -0700

    Reformat plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,21 +1,23 @@
 local gh = function(path) return 'https://github.com/' .. path end
 
 vim.pack.add({
-    gh('nvim-lua/plenary.nvim'),
-    gh('nvim-telescope/telescope.nvim'),
-    gh('nvim-telescope/telescope-ui-select.nvim'),
-    gh('nvim-telescope/telescope-fzf-native.nvim'),
+    gh'nvim-lua/plenary.nvim',
+    gh'nvim-tree/nvim-web-devicons',
 
-    gh('romus204/tree-sitter-manager.nvim'),
-    { src = gh('nvim-treesitter/nvim-treesitter-textobjects'), version = 'main', },
+    gh'nvim-telescope/telescope.nvim',
+    gh'nvim-telescope/telescope-ui-select.nvim',
+    gh'nvim-telescope/telescope-fzf-native.nvim',
 
-    gh('neovim/nvim-lspconfig'),
-    gh('mason-org/mason.nvim'),
-    gh('mason-org/mason-lspconfig.nvim'),
-    gh('seblyng/roslyn.nvim'),
+    gh'romus204/tree-sitter-manager.nvim',
+    { src = gh'nvim-treesitter/nvim-treesitter-textobjects', version = 'main', },
 
-    { src = gh('catppuccin/nvim'), name = 'catppuccin-nvim', },
-    gh('folke/tokyonight.nvim'),
+    gh'neovim/nvim-lspconfig',
+    gh'mason-org/mason.nvim',
+    gh'mason-org/mason-lspconfig.nvim',
+    gh'seblyng/roslyn.nvim',
+
+    { src = gh'catppuccin/nvim', name = 'catppuccin-nvim', },
+    gh'folke/tokyonight.nvim',
 })
 
 require('plugins.telescope')
