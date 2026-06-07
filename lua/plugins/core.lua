1598fa5c7d9c3cf843c776828ca2ebe9c6dd61e8
Grendel <jcg@grendel.world>
2026-06-07 13:42:08 -0700

    Add nvim-autopairs

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,5 +2,17 @@ return {
     {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
-    }
+        'windwp/nvim-autopairs',
+    },
+
+    config = function()
+        require('nvim-autopairs').setup({
+            enable_autoquote = true,
+            check_ts = true,
+            ts_config = {
+                lua = { 'string' },
+                java = false,
+            },
+        })
+    end
 }
