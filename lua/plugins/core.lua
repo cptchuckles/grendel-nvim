acfe1cf1b467f57bd299f75a5bd25cd3e598c6f0
Grendel <jcg@grendel.world>
2026-06-23 14:32:04 -0700

    Add gitsigns to core

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -4,6 +4,7 @@ return {
         'nvim-tree/nvim-web-devicons',
         'windwp/nvim-autopairs',
         'NMAC427/guess-indent.nvim',
+        'lewis6991/gitsigns.nvim',
     },
 
     config = function()
@@ -14,6 +15,27 @@ return {
                 lua = { 'string' },
                 java = false,
             },
+        })
+
+        require('gitsigns').setup({
+            current_line_blame = true,
+            current_line_blame_opts = {
+                delay = 200,
+            },
+
+            on_attach = function(bufnr)
+                local gs = require('gitsigns')
+                vim.keymap.set('n', '[c', function() gs.nav_hunk('prev', { preview = true}) end, { buf = bufnr, desc = 'Git jump to previous hunk' })
+                vim.keymap.set('n', ']c', function() gs.nav_hunk('next', { preview = true}) end, { buf = bufnr, desc = 'Git jump to next hunk' })
+                vim.keymap.set('n', '<leader>gs', gs.stage_hunk, { buf = bufnr, desc = 'Git stage hunk' })
+                vim.keymap.set('n', '<leader>gr', gs.reset_hunk, { buf = bufnr, desc = 'Git reset hunk' })
+                vim.keymap.set('n', '<leader>gg', function() gs.setloclist(0, "all") end, { buf = bufnr, desc = 'Git hunks to loclist' })
+                vim.keymap.set('n', '<leader>gS', gs.stage_buffer, { buf = bufnr, desc = 'Git stage buffer' })
+                vim.keymap.set('n', '<leader>gR', gs.reset_buffer, { buf = bufnr, desc = 'Git reset buffer' })
+                vim.keymap.set('n', '<leader>gB', gs.toggle_current_line_blame, { buf = bufnr, desc = 'Git toggle inline blame' })
+                vim.keymap.set('n', '<leader>gb', function() gs.blame_line({ full = true }) end, { buf = bufnr, desc = 'Git show current line blame' })
+                vim.keymap.set('n', '<leader>gp', gs.preview_hunk, { buf = bufnr, desc = 'Git preview hunk' })
+            end,
         })
 
         require('guess-indent').setup()
