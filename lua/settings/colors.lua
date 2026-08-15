dabdbd389382a86fc1847ebbfcdae2f734a80577
Grendel <jcg@grendel.world>
2026-08-14 22:03:05 -0700

    Fix FloatBorder highlight group for ClearBg

--- lua/settings/colors.lua
+++ lua/settings/colors.lua
@@ -19,6 +19,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
             vim.api.nvim_set_hl(0, "Normal", { bg = 'none' })
             vim.api.nvim_set_hl(0, "NormalNC", { bg = 'none' })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = 'none' })
+            vim.api.nvim_set_hl(0, "FloatBorder", { bg = 'none' })
             vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = 'none' })
             vim.api.nvim_set_hl(0, "Pmenu", { bg = 'none' })
         end
