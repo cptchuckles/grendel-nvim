b173b8f6172c17b655a13fe2320eed1319a506c4
Grendel <jcg@grendel.world>
2026-08-16 12:37:31 -0700

    Add easier completion mappings

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -16,6 +16,9 @@ return {
 
             keymap = {
                 preset = 'default',
+                ['<CR>'] = { 'accept', 'fallback' },
+                ['<Tab>'] = { 'select_next', 'fallback' },
+                ['<S-Tab>'] = { 'select_prev', 'fallback' },
             },
             appearance = {
                 nerd_font_variant = 'normal',
